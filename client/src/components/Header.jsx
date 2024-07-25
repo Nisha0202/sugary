464a317f4a5b8fb77efe2fd42a6f7f6a63459453
Nisha0202 <nishajabatunnessa@gmail.com>
2024-07-25 07:02:03 +0600

    basic setup + navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,8 +6,9 @@ export default function Header() {
 
     <div className="navbar bg-base-100">
   <div className="navbar-start">
+    {/* menu */}
     <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+      <div tabIndex={0} role="button" className="mr-2 lg:hidden">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-5 w-5"
@@ -24,36 +25,21 @@ export default function Header() {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+           <li><a>Home</a></li>
+           <li><a>My Orders</a></li>
       </ul>
     </div>
     <a className="text-xl text-pink-600 font-semibold">Sugary</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
+      <li><a>Home</a></li>
+      <li><a>My Orders</a></li>
       <li><a>Item 3</a></li>
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn btn-sm text-green-600 tracking-wider rounded-md">Login</a>
   </div>
 </div>
     </>
