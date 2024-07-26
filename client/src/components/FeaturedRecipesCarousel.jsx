9b3d82c8c7ba19555daa7bceddf8b075560d1a70
Nisha0202 <nishajabatunnessa@gmail.com>
2024-07-26 07:23:58 +0600

    menu page

--- client/src/components/FeaturedRecipesCarousel.jsx
+++ client/src/components/FeaturedRecipesCarousel.jsx
@@ -1,28 +1,27 @@
-/* eslint-disable @next/next/no-img-element */
-'use client'
+
 import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './FeaturedRecipesCarousel.css'; // Import your CSS file for styling
-import Link from 'next/link';
+import { NavLink, Link } from 'react-router-dom'
 
 
 const FeaturedRecipesCarousel = () => {
 
-const [recipes, setRecipes] = useState([]);
+const [cupcakes, setCupcakes] = useState([]);
 
   useEffect(() => {
-    fetch('recipes.json')
+    fetch('cupcakes.json')
      .then(response => response.json())
-     .then(data => setRecipes(data));
+     .then(data => setCupcakes(data));
   }, []);
 
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3, // Display 3 recipes per slide
+    slidesToShow: 3, // Display 3 cupcakes per slide
     slidesToScroll: 1,
     swipeToSlide: true, // Enables swipe to slide
     autoplay: true,
@@ -42,19 +41,19 @@ const [recipes, setRecipes] = useState([]);
   return (
     <div className='max-w-[1200px] mx-auto mt-8 lg:mt-12 mb-16 lg:mb-24'>
       <Slider {...settings}>
-        {recipes.map(recipe => (
-          <div key={recipe._id} className='border-0 outline-none'>
-            <Link href={`/recipes/${recipe._id}`} legacyBehavior>
+        {cupcakes.map(cupcake => (
+          <div key={cupcake._id} className='border-0 outline-none'>
+            <Link to={"/menu"} legacyBehavior>
                 <img
                   className="carousel-image cursor-pointer rounded-md border-2 outline-none hover:opacity-70"
-                  src={recipe.imageUrl}
-                  alt={recipe.title}
+                  src={cupcake.imageUrl}
+                  alt={cupcake.title}
                 />
     
             </Link>
             <div className="carousel-content grid place-content-center">
-              <h3 className='font-bold text-lg text-text'>{recipe.title}</h3>
-              <p className='text-wrap min-w-80 px-4'>{recipe.description}</p>
+              <h3 className='font-bold text-lg text-text'>{cupcake.title}</h3>
+              <p className='text-wrap min-w-80 px-4'>{cupcake.description}</p>
             </div>
           </div>
         ))}
--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../components/Card';
 
 const Menu = () => {
-    const cupcake = {
-        title: 'Delicious Cupcake',
-        description: 'A deliciously sweet and moist cupcake, perfect for any occasion.',
-        image: 'https://i.pinimg.com/564x/91/33/ea/9133ea1d7738ac492706a7bba1b2fa6a.jpg', // Replace with your image path
-        pricePerSix: 15,
-        pricePerTwelve: 28
-    };
+    // const cupcake = {
+    //     title: 'Delicious Cupcake',
+    //     description: 'A deliciously sweet and moist cupcake, perfect for any occasion.',
+    //     image: 'https://i.pinimg.com/564x/91/33/ea/9133ea1d7738ac492706a7bba1b2fa6a.jpg', // Replace with your image path
+    //     pricePerSix: 15,
+    //     pricePerTwelve: 28
+    // };
+
+
+    const [cupcakes, setCupcakes] = useState([]);
+
+    useEffect(() => {
+        fetch('cupcakes.json')
+            .then(response => response.json())
+            .then(data => setCupcakes(data));
+    }, []);
 
     return (
-        <div className="flex flex-col gap-4 justify-center items-center h-auto mt-12 lg:mt-20"> 
-        <section>
-                <h2 className="text-2xl w-full font-semibold text-center text-primary">Featured Recepies</h2>
+        <div className="flex flex-col gap-4 justify-center items-center h-auto mt-12 lg:mt-20">
+            <section>
+                <h2 className="text-2xl w-full font-semibold text-center text-primary">Our Menu</h2>
             </section>
-        <div className="flex flex-wrap gap-4 justify-center items-center h-auto my-8">
-            
-            <Card cupcake={cupcake} />
+            <div className="flex flex-wrap gap-4 justify-center items-center h-auto my-8">
+                {cupcakes.map((cupcake, index) => (
+                    <Card key={index} cupcake={cupcake} />
+                ))}
+                {/* <Card cupcake={cupcake} /> */}
+            </div>
         </div>
-        </div>
-       
+
     );
 };
 
