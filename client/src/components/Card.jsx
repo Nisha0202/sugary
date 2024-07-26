f0785521a05d0a5b75785ac4a52e20d8756553fb
Nisha0202 <nishajabatunnessa@gmail.com>
2024-07-27 03:29:09 +0600

    Card styled

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { IoMdArrowDropdown } from "react-icons/io";
 
 const Card = ({ cupcake }) => {
     const [boxSize, setBoxSize] = useState(6);
@@ -28,21 +29,25 @@ const Card = ({ cupcake }) => {
         <div className="w-72 rounded-md flex justify-center items-center text-center flex-col border-2 overflow-hidden p-4 bg-white">
             <img className="w-full h-52 rounded-md object-cover border-2 " src={cupcake.image} alt={cupcake.title} />
             <div className="py-4">
-                <div className="font-medium tracking-wide text-xl h-6">{cupcake.title}</div>
+                <div className='w-full justify-between flex items-center h-6 px-2'>
+                    <div className="font-medium tracking-wide text-xl ">{cupcake.title}</div>
+                    <div className='text-sm'>{cupcake.weight}g each</div>
+                </div>
+
                 <div className="text-sm tracking-wider mt-4 h-14">{cupcake.description}</div>
             </div>
-            <div className="">
+            <div className="px-2 w-full">
                 {/* selection */}
-                <div className='flex justify-between items-center w-full gap-6 font-medium'>
+                <div className='flex w-full justify-between items-center gap-8 font-medium '>
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm  mb-2" htmlFor="boxSize">
-                            Select Box Size:
+                            Box Size:
                         </label>
                         <div className="relative">
                             <select
                                 id="boxSize"
-                                className="block input input-sm text-sm appearance-none w-full bg-white border border-gray-400 hover:border-gray-500
-                                 rounded-sm leading-tight focus:outline-none focus:shadow-outline"
+                                className="block p-2 min-w-24 text-sm appearance-none bg-white border border-gray-400 hover:border-gray-500
+                                 rounded leading-tight focus:outline-none focus:shadow-outline"
                                 value={boxSize}
                                 onChange={handleBoxSizeChange}
                             >
@@ -50,22 +55,20 @@ const Card = ({ cupcake }) => {
                                 <option value={12}>12 pieces</option>
                             </select>
                             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path d="M7 10l5 5 5-5H7z" />
-                                </svg>
+                                <IoMdArrowDropdown className='text-sm' />
                             </div>
                         </div>
                     </div>
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm  mb-2" htmlFor="quantity">
-                            Select Quantity of Box:
+                            Quantity of Box:
                         </label>
                         <input
                             type="number"
                             id="quantity"
                             min="1" max={"6"}
-                            className="block input input-sm appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 
-                            rounded-sm leading-tight focus:outline-none focus:shadow-outline"
+                            className="block p-2 appearance-none text-sm w-24 bg-white border border-gray-400 hover:border-gray-500 
+                            rounded leading-tight focus:outline-none focus:shadow-outline"
                             value={quantity}
                             onChange={handleQuantityChange}
                         />
@@ -73,10 +76,10 @@ const Card = ({ cupcake }) => {
                 </div>
                 {/* result */}
                 <div>
-                    <p className="text-sm ">Total Boxes: {quantity} {boxSize === 6 ? "(Regular)" : "(Large)"}</p>
+                    {/* <p className="text-sm ">Total Boxes: {quantity} {boxSize === 6 ? "(Regular)" : "(Large)"}</p>
                     <p className="text-sm mt-2">Total Quantity: {quantity*boxSize} pieces <span>{cupcake.weight}g each</span></p>
-                    
-                    <p className="mt-2  ">Total Price: <span className=' text-secondary font-semibold'>{(price * quantity).toFixed(2)}Tk</span></p>
+                     */}
+                    <p className="mt-2  ">Total Price: <span className=' text-pink-500 font-semibold'>{(price * quantity).toFixed(2)}Tk</span></p>
                 </div>
                 {/* action */}
                 <button className="mt-4 w-full border-2 border-green-600 text-green-600 tracking-wide hover:bg-gray-100 font-bold py-2 px-4 rounded">
